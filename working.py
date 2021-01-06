a530756d068e56cf08ab3e541993e94bfc8e4ca9
Nitesh Prajapat <nitesh.19jdml019@jietjodhpur.ac.in>
2021-01-07 01:14:04 +0530

    user interaction file feature added

--- working.py
+++ working.py
@@ -1,4 +1,5 @@
 import bot
+import pyautogui
 
 print("1. Signup")
 print("2. Start Meeting")
@@ -9,7 +10,9 @@ user_choice = int(input("Enter your Choice good friend!! "))
 
 if __name__ == '__main__':
     if user_choice == 1:
-        bot.sign_in()
+        email = pyautogui.prompt("Enter your Email to sign in ")
+        pwd = pyautogui.prompt("Enter your Password to sign in ")
+        bot.sign_in(email, pwd)
 
     elif user_choice == 2:
         bot.LETS_START_MEETING()
